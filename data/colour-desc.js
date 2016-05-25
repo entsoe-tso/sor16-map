d0e7327908fb99fcc19194c465e8b234bc12611f
Colin Broderick <rustybroderick@gmail.com>
2016-05-25 16:26:32 +0200

    initial commit

---
+++ data/colour-desc.js
@@ -0,0 +1,18 @@
+[
+  {
+    "colour": "1",
+    "desc": "No import needed"
+  },
+  {
+    "colour": "2",
+    "desc": "Import needed for some weeks but enough interconnection capacity available"
+  },
+  {
+    "colour": "3",
+    "desc": "Import needed all summer long but enough interconnection capacity available"
+  },
+  {
+    "colour": "4",
+    "desc": "Import needed for most of the summer. There is a risk of not enough import capacity being available on certain interconnections during certain weeks"
+  }
+]
